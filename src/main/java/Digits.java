7add459c7f3b85e05b3addddee715f1d8ffe9577
Justin <justinlei471@gmail.com>
2025-04-09 09:27:50 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -9,7 +9,7 @@ public class Digits
 	{ /* to be implemented in part (a) */ 
 	    digitList= new ArrayList<Integer>();
 		while(num!=0){
-			digitList.add(0,num/10);
+			digitList.add(0,num%10);
 			num=num/10;
 		}
 	}
